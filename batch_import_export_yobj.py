fc732a7c6c5802ff88addfaaec73e019983d3d6c
l1145a1 <la45project@gmail.com>
2025-06-06 13:05:42 +0700

    Add files via upload

--- batch_import_export_yobj.py
+++ batch_import_export_yobj.py
@@ -13,19 +13,16 @@ class hex_file:
     def read_int(self, offset, size):
         self.file_obj.seek(offset)
         result = int.from_bytes(self.file_obj.read(size), byteorder='little')
-        print(f"Read Int at offset {offset}, size {size}: {result}")
         return result
 
     def read_string(self, offset, size):
         self.file_obj.seek(offset)
         result = self.file_obj.read(size).decode('utf-8').rstrip('\x00')
-        print(f"Read String at offset {offset}, size {size}: {result}")
         return result
 
     def read_float(self, offset, size):
         self.file_obj.seek(offset)
         result = struct.unpack('f', self.file_obj.read(size))[0]
-        print(f"Read Float at offset {offset}, size {size}: {result}")
         return result
 
     def float_to_string(self, value, byte_size=4):
@@ -40,7 +37,6 @@ class hex_file:
     def write_string(self, offset, value):
         self.file_obj.seek(offset)
         self.file_obj.write(value)
-        print(f"Write String at offset {offset}: {value}")
 
 # Implementasi string_shortener function
 def string_shortener(s):
@@ -57,111 +53,147 @@ def rotate_3d_x(x, y, z, degree):
 class YOBJReader:
     def __init__(self, filename):
         self.filename = filename
+        print(f"Read File: {self.filename}")
         self.file_obj = open(filename, 'rb+')
         self.file = hex_file(self.file_obj)
+
         self.offset_to_pof0 = self.file.read_int(4, 4) + 8
+        print(f"Read POF0 offset: {self.offset_to_pof0}")
+
         self.size_entry = self.offset_to_pof0 - 8
-        self.n_mesh = self.file.read_int(24, 4)
-        if self.n_mesh > 2147483647:
-            self.n_mesh = self.file.read_int(52, 4)
-        self.n_texture = self.file.read_int(32, 4)
+        print(f"Read Size Entry?: {self.size_entry}")
+
+        self.mesh_count = self.file.read_int(24, 4)
+        if self.mesh_count > 2147483647:
+            self.mesh_count = self.file.read_int(52, 4)
+        print(f"Read Mesh Count: {self.mesh_count}")
+
+        self.texture_count = self.file.read_int(32, 4)
+        print(f"Read Texture Count: {self.texture_count}")
+
         self.texture_start_offset = self.file.read_int(44, 4) + 8
-        self.texture_end_offset = self.texture_start_offset + 16 * self.n_texture
+        self.texture_end_offset = self.texture_start_offset + 16 * self.texture_count
+        print(f"Read Texture Start Offset: {self.texture_start_offset}")
+        print(f"Read Texture End Offset: {self.texture_end_offset}")
+
+        print(f"Get Texture:")
         self.texture_list = self.get_texture_list()
-        self.n_bone = self.file.read_int(28, 4)
-        self.offset_to_mesh_description = self.file.read_int(36, 4)
-        self.offset_to_bone = self.file.read_int(40, 4) + 8
-        self.mesh_material_number = []
-        self.offset_to_indice_section = []
-        self.n_bone_weight_of_mesh = []
+
+        self.bone_count = self.file.read_int(28, 4)
+        print(f"Read Bone Count: {self.bone_count}")
+
+        self.mesh_header_offset = self.file.read_int(36, 4)
+        print(f"Read Mesh Header Offset: {self.mesh_header_offset}")
+
+        self.bone_offset = self.file.read_int(40, 4) + 8
+        print(f"Read Bone Offset: {self.bone_offset}")
+
+        self.mesh_material_count = []
+        self.offset_to_material = []
         self.offset_to_vertex_data = []
-        self.n_vertices_of_mesh = []
-        self.offset_to_normal_data = []
-        self.relative_offset = []
-        self.mesh_blocksize = []
+        self.mesh_vertices_count = []
+        self.mesh_vertice_lenght = []
         self.mesh_flag = []
         self.offset_to_uv_data = []
-        for x in range(self.n_mesh):
-            value = self.file.read_int(self.offset_to_mesh_description + 36 + 64 * x, 4)
+        print(f"Read Mesh Header:")
+        for x in range(self.mesh_count):
+            print(f"Object {x}")
+            value = self.file.read_int(self.mesh_header_offset + 36 + 64 * x, 4)
+            print(f"Read Mesh Flag: {value}")
             flag = value & 1536 != 0
+            print(f"Read Mesh Flag Boolean: {flag}")
             binary_val = format(value, '032b')[::-1]
+            print(f"Read Mesh Flag Binary: {binary_val}")
             value1 = int(binary_val[16:13:-1], 2)
-            offset = self.file.read_int(self.offset_to_mesh_description + 32 + 64 * x, 4) + 8
+            print(f"Read Mesh Flag Integer: {value1}")
+            offset = self.file.read_int(self.mesh_header_offset + 32 + 64 * x, 4) + 8
             offset = self.file.read_int(offset, 4) + 8
+            print(f"Read Mesh Offset: {offset}")
             if flag:
                 uv_offset = offset + (value1 + 1) * 4
             else:
                 uv_offset = offset
+            print(f"Read UV Offset: {uv_offset}")
             self.offset_to_uv_data.append(uv_offset)
             if flag:
                 vertex_offset = offset + (value1 + 1 + 3) * 4
             else:
                 vertex_offset = offset + 12
+            print(f"Read Vertex Offset: {vertex_offset}")
             self.offset_to_vertex_data.append(vertex_offset)
             if flag:
-                self.mesh_blocksize.append((value1 + 10) * 4)
+                self.mesh_vertice_lenght.append((value1 + 10) * 4)
             else:
-                self.mesh_blocksize.append(36)
+                self.mesh_vertice_lenght.append(36)
+            print(f"Read Vertice Lenght: {self.mesh_vertice_lenght[x]}")
             self.mesh_flag.append(flag)
-            self.n_vertices_of_mesh.append(self.file.read_int(self.offset_to_mesh_description + 48 + 64 * x, 4))
-            self.offset_to_indice_section.append(self.file.read_int(self.offset_to_mesh_description + 20 + 64 * x, 4) + 8)
-            self.mesh_material_number.append(self.file.read_int(self.offset_to_mesh_description + 12 + 64 * x, 4))
-            self.n_bone_weight_of_mesh.append(self.file.read_int(self.offset_to_mesh_description + 44 + 64 * x, 4))
-            self.relative_offset.append(self.file.read_int(self.offset_to_mesh_description + 60 + 64 * x, 4))
+            self.mesh_vertices_count.append(self.file.read_int(self.mesh_header_offset + 48 + 64 * x, 4))
+            print(f"Read Vertice Count: {self.mesh_vertices_count[x]}")
+            self.offset_to_material.append(self.file.read_int(self.mesh_header_offset + 20 + 64 * x, 4) + 8)
+            print(f"Read Offset To Material: {self.offset_to_material[x]}")
+            self.mesh_material_count.append(self.file.read_int(self.mesh_header_offset + 12 + 64 * x, 4))
+            print(f"Read Mesh Material Count: {self.mesh_material_count[x]}")
 
     def get_vertex_data(self, mesh_id):
+        print(f"Get Vertex Data: Object {mesh_id}")
         vertex_list = []
         offset = self.offset_to_vertex_data[mesh_id]
-        block_size = self.mesh_blocksize[mesh_id]
-        for x in range(self.n_vertices_of_mesh[mesh_id]):  # Ubah xrange ke range
-            f1 = self.file.read_float(offset + block_size * x + 12, 4)
-            f2 = self.file.read_float(offset + block_size * x + 16, 4)
-            f3 = self.file.read_float(offset + block_size * x + 20, 4)
-            vertex_x = f1
-            vertex_y = f2
-            vertex_z = f3
+        vertice_lenght = self.mesh_vertice_lenght[mesh_id]
+        for x in range(self.mesh_vertices_count[mesh_id]):  # Ubah xrange ke range
+            vertex_x = self.file.read_float(offset + vertice_lenght * x + 12, 4)
+            vertex_y = self.file.read_float(offset + vertice_lenght * x + 16, 4)
+            vertex_z = self.file.read_float(offset + vertice_lenght * x + 20, 4)
+            print(f"Vertice {x}: x {vertex_x}, y {vertex_y}, z {vertex_z}")
             vertex_list.append([vertex_x, vertex_y, vertex_z])
         return vertex_list
 
     def get_uv_data(self, mesh_id):
+        print(f"Get UV Data: Object {mesh_id}")
         uv_list = []
         offset = self.offset_to_uv_data[mesh_id]
-        block_size = self.mesh_blocksize[mesh_id]
-        for x in range(self.n_vertices_of_mesh[mesh_id]):
-            f1 = self.file.read_float(offset + block_size * x + 0, 4)
-            f2 = self.file.read_float(offset + block_size * x + 4, 4)
-            uv_x = f1
-            uv_y = f2
+        vertice_lenght = self.mesh_vertice_lenght[mesh_id]
+        for x in range(self.mesh_vertices_count[mesh_id]):
+            uv_x = self.file.read_float(offset + vertice_lenght * x + 0, 4)
+            uv_y = self.file.read_float(offset + vertice_lenght * x + 4, 4)
+            print(f"UV {x}: x {uv_x}, y {uv_y}")
             uv_list.append([uv_x, uv_y])
         return uv_list
 
     def get_face_data(self, mesh_id):
+        print(f"Get Face Data: Object {mesh_id}")
         face_list = []
-        offset = self.offset_to_indice_section[mesh_id]
-        nMat = self.mesh_material_number[mesh_id]
+        offset = self.offset_to_material[mesh_id]
+        material_count = self.mesh_material_count[mesh_id]
         face_data = []
-        for x in range(0, nMat):
-            n_indices = self.file.read_int(offset + 132, 4)
-            start_indices = self.file.read_int(offset + 136, 4) + 8
-            face_data.append([n_indices, start_indices])
+        for x in range(0, material_count):
+            print(f"Material {x}")
+            faces_header_count = self.file.read_int(offset + 132, 4)
+            print(f"Face Header Count: {faces_header_count}")
+            faces_header_start = self.file.read_int(offset + 136, 4) + 8
+            print(f"Face Header Start: {faces_header_start}")
+            face_data.append([faces_header_count, faces_header_start])
             offset += 144
 
         for i, j in face_data:
             for x in range(i):
-                nFaces = self.file.read_int(j + 8 + 16 * x, 4)
-                face_address = self.file.read_int(j + 12 + 16 * x, 4) + 8
-                offset = face_address
+                face_count = self.file.read_int(j + 8 + 16 * x, 4)
+                face_offset = self.file.read_int(j + 12 + 16 * x, 4) + 8
+                print(f"Face {x}: Count {face_count}, Offset {face_offset}")
+                offset = face_offset
                 v1 = -1
                 v2 = -1
                 direct = -1
-                for y in range(nFaces):
+                for y in range(face_count):
                     direct *= -1
                     v3 = self.file.read_int(offset, 2)
+                    print(f"Face {y}: direct {direct}, v1 {v1}, v2 {v2}, v3 {v3}")
                     if v1 != -1 and v2 != -1:
                         if direct > 0:
                             face_list.append([v1, v2, v3])
+                            print(f"[{v1+1}, {v2+1}, {v3+1}]")
                         else:
                             face_list.append([v1, v3, v2])
+                            print(f"[{v1+1}, {v3+1}, {v2+1}]")
                     v1 = v2
                     v2 = v3
                     offset += 2
@@ -170,32 +202,38 @@ class YOBJReader:
 
     def get_face_data_log(self, mesh_id):
         face_list = []
-        offset = self.offset_to_indice_section[mesh_id]
-        nMat = self.mesh_material_number[mesh_id]
+        offset = self.offset_to_material[mesh_id]
+        material_count = self.mesh_material_count[mesh_id]
         face_data = []
-        for x in range(0, nMat):
-            n_indices = self.file.read_int(offset + 132, 4)
-            start_indices = self.file.read_int(offset + 136, 4) + 8
-            face_data.append([n_indices, start_indices])
+        for x in range(0, material_count):
+            faces_header_count = self.file.read_int(offset + 132, 4)
+            print(f"Face Header Count: {faces_header_count}")
+            faces_header_start = self.file.read_int(offset + 136, 4) + 8
+            print(f"Face Header Start: {faces_header_start}")
+            face_data.append([faces_header_count, faces_header_start])
             offset += 144
 
         for i, j in face_data:
             temp_face = []
             for x in range(i):
-                nFaces = self.file.read_int(j + 8 + 16 * x, 4)
-                face_address = self.file.read_int(j + 12 + 16 * x, 4) + 8
-                offset = face_address
+                face_count = self.file.read_int(j + 8 + 16 * x, 4)
+                face_offset = self.file.read_int(j + 12 + 16 * x, 4) + 8
+                print(f"Face {x}: Count {face_count}, Offset {face_offset}")
+                offset = face_offset
                 v1 = -1
                 v2 = -1
                 direct = -1
-                for y in range(nFaces):
+                for y in range(face_count):
                     direct *= -1
                     v3 = self.file.read_int(offset, 2)
+                    print(f"Face {y}: direct {direct}, v1 {v1}, v2 {v2}, v3 {v3}")
                     if v1 != -1 and v2 != -1:
                         if direct > 0:
                             temp_face.append([v1, v2, v3])
+                            print(f"[{v1+1}, {v2+1}, {v3+1}]")
                         else:
                             temp_face.append([v1, v3, v2])
+                            print(f"[{v1+1}, {v3+1}, {v2+1}]")
                     v1 = v2
                     v2 = v3
                     offset += 2
@@ -205,22 +243,26 @@ class YOBJReader:
         return face_list
 
     def get_texture_pointed(self, mesh_id):
-        nMat = self.mesh_material_number[mesh_id]
-        offset = self.offset_to_indice_section[mesh_id]
+        print(f"Read Object {mesh_id} Texture")
+        material_count = self.mesh_material_count[mesh_id]
+        offset = self.offset_to_material[mesh_id]
         texture_list = []
-        for x in range(nMat):
+        for x in range(material_count):
             texture_id = self.file.read_int(offset + 22, 2)
             texture_list.append(texture_id)
+            print(f"Material {x}, Texture: {texture_id}")
             offset += 144
 
         return texture_list
 
     def get_unknown_float(self, mesh_id):
-        nMat = self.mesh_material_number[mesh_id]
-        offset = self.offset_to_indice_section[mesh_id]
+        print(f"Read Object {mesh_id} Unknown Float")
+        material_count = self.mesh_material_count[mesh_id]
+        offset = self.offset_to_material[mesh_id]
         float_list = []
-        for x in range(nMat):
+        for x in range(material_count):
             unknown_float = self.file.read_float(offset + 16, 4)
+            print(f"Material {x}, Value: {unknown_float} * 0.01 = {unknown_float *0.01}")
             unknown_float *= 0.01
             float_list.append(unknown_float)
             offset += 144
@@ -228,35 +270,42 @@ class YOBJReader:
         return float_list
 
     def get_face_data_and_tex(self, mesh_id):
+        print(f"Read Object {mesh_id} Faces")
         face_dict = OrderedDict()
-        offset = self.offset_to_indice_section[mesh_id]
-        nMat = self.mesh_material_number[mesh_id]
+        offset = self.offset_to_material[mesh_id]
+        material_count = self.mesh_material_count[mesh_id]
         texture_list = self.get_texture_pointed(mesh_id)
         float_list = self.get_unknown_float(mesh_id)
         face_data = []
-        for x in range(0, nMat):
-            n_indices = self.file.read_int(offset + 132, 4)
-            start_indices = self.file.read_int(offset + 136, 4) + 8
-            face_data.append([n_indices, start_indices])
+        for x in range(0, material_count):
+            faces_header_count = self.file.read_int(offset + 132, 4)
+            print(f"Face Header Count: {faces_header_count}")
+            faces_header_start = self.file.read_int(offset + 136, 4) + 8
+            print(f"Face Header Start: {faces_header_start}")
+            face_data.append([faces_header_count, faces_header_start])
             offset += 144
 
         for z, (i, j) in enumerate(face_data):
             face_list = []
             for x in range(i):
-                nFaces = self.file.read_int(j + 8 + 16 * x, 4)
-                face_address = self.file.read_int(j + 12 + 16 * x, 4) + 8
-                offset = face_address
+                face_count = self.file.read_int(j + 8 + 16 * x, 4)
+                face_offset = self.file.read_int(j + 12 + 16 * x, 4) + 8
+                print(f"Face {x}: Count {face_count}, Offset {face_offset}")
+                offset = face_offset
                 v1 = -1
                 v2 = -1
                 direct = -1
-                for y in range(nFaces):
+                for y in range(face_count):
                     direct *= -1
                     v3 = self.file.read_int(offset, 2)
+                    print(f"Face {y}: direct {direct}, v1 {v1}, v2 {v2}, v3 {v3}")
                     if v1 != -1 and v2 != -1:
                         if direct:
                             face_list.append([v1, v2, v3])
+                            print(f"[{v1+1}, {v2+1}, {v3+1}]")
                         else:
                             face_list.append([v1, v3, v2])
+                            print(f"[{v1+1}, {v3+1}, {v2+1}]")
                     v1 = v2
                     v2 = v3
                     offset += 2
@@ -268,8 +317,8 @@ class YOBJReader:
 
     def get_obj_details(self):
         obj_details = []
-        for i in range(self.n_mesh):
-            vertex_count = self.n_vertices_of_mesh[i]
+        for i in range(self.mesh_count):
+            vertex_count = self.mesh_vertices_count[i]
             obj_details.append(f"Object {i}. Vertice Count: {vertex_count}")
         return obj_details
 
@@ -294,7 +343,7 @@ class YOBJReader:
             mat_count = 0
             texture_list = self.get_texture_list()
 
-            for mesh_id in range(self.n_mesh):
+            for mesh_id in range(self.mesh_count):
                 obj_file.write(f'# Mesh {mesh_id}\n')
                 vertex_list = self.get_vertex_data(mesh_id)
                 for vertex in vertex_list:
@@ -346,13 +395,13 @@ class YOBJReader:
         normal_list = obj_file.normal
         face_list = obj_file.face  # Pastikan face tidak berubah
         offset = self.offset_to_vertex_data[mesh_id]
-        block_size = self.mesh_blocksize[mesh_id]
+        vertice_lenght = self.mesh_vertice_lenght[mesh_id]
 
-        if len(vertex_list) != self.n_vertices_of_mesh[mesh_id]:
+        if len(vertex_list) != self.mesh_vertices_count[mesh_id]:
             raise AssertionError("This function only supports injecting OBJ with the same amount of vertices")
 
         # Inject vertex data
-        for x in range(self.n_vertices_of_mesh[mesh_id]):
+        for x in range(self.mesh_vertices_count[mesh_id]):
             vertex_x, vertex_y, vertex_z = rotate_3d_x(vertex_list[x][0], vertex_list[x][2], -vertex_list[x][1], -180)
             vertex_offset = offset + 12  # Pastikan offset tetap akurat
             data_v_x = self.file.float_to_string(vertex_x, 4)
@@ -361,7 +410,7 @@ class YOBJReader:
             self.file.write_string(vertex_offset, data_v_x)
             self.file.write_string(vertex_offset + 4, data_v_y)
             self.file.write_string(vertex_offset + 8, data_v_z)
-            offset += block_size
+            offset += vertice_lenght
 
         # UV tidak disentuh, pastikan tidak diubah
         # Jika masih rusak, cek apakah face_list tetap sama setelah inject
@@ -371,7 +420,7 @@ class YOBJReader:
 
 
     def get_object_count(self):
-        return self.n_mesh
+        return self.mesh_count
 
 class YOBJ_GUI:
     def __init__(self, root):
